4c221150518d804085baca13f2b078428ec6ab3d
Luchillo <luchillo17@gmail.com>
2016-10-01 10:55:36 -0500

    Fix app height, finish start of WebGl

--- src/app/+graphics/webgl/webgl.component.ts
+++ src/app/+graphics/webgl/webgl.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 import { AppState } from '../../app.service';
 
-import * as README from '../../../README';
 @Component({
   selector: 'webgl',
   providers: [
   ],
-  // styleUrls: [ './webgl.style.scss' ],
+  styleUrls: [ './webgl.style.scss' ],
   templateUrl: './webgl.template.html'
 })
 export class WebGl {
-  // Set our default values
+  @ViewChild('canvas') canvasRef: ElementRef;
   localState = { value: '' };
+
+  private canvas: HTMLCanvasElement;
+
+  private webglContext: WebGLRenderingContext;
   constructor(public appState: AppState) {
   }
 
   ngOnInit() {
     console.log('hello `webgl` component');
+    this.canvas = this.canvasRef.nativeElement;
+
+    this.webglContext = <WebGLRenderingContext>this.canvas.getContext('webgl');
+    this.webglContext.viewport(0, 0, this.canvas.width, this.canvas.height);
   }
 }
